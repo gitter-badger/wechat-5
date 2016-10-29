f4dd313eaebeaca3de9a1542d7e67e6544d2bac6
Nico W. J <gksqlsdy@gmail.com>
2016-10-30 05:21:36 +0800

    Add oauth2

--- web/controller/wechat.go
+++ web/controller/wechat.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/alecthomas/log4go"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"nicosoft.org/wechat/web/service"
@@ -31,4 +32,19 @@ func WeChatOauth2(c *gin.Context) {
 
 func OAuth2Handler(c *gin.Context) {
 
+	code := c.Param("code")
+	state := c.Param("state")
+
+	log4go.Debug("OAuth2 code: %v,state: %v", code, state)
+
+	if code == "" {
+		c.String(http.StatusOK, "%v", "Request Bad.")
+		return
+	}
+
+	user := new(wechat.UserInfo)
+	token := user.GetToken(code)
+	result := user.GetUserInfo(token.OpenId, token.Token)
+
+	c.JSON(http.StatusOK, result)
 }
---
+++ wechat/userinfo.go
@@ -0,0 +1,93 @@
+package wechat
+
+import (
+	"encoding/json"
+	"github.com/alecthomas/log4go"
+	"io/ioutil"
+	"net/http"
+	"nicosoft.org/wechat/utils"
+	"strings"
+)
+
+type UserToken struct {
+	Token     string `json:"access_token"`
+	ExpiresIn int    `json:"expires_in"`
+	NewToken  string `json:"refresh_token"`
+	OpenId    string `json:"openid"`
+	Scope     string `json:"scope"`
+}
+
+type UserInfo struct {
+	openid     string   `json:"openid"`
+	nickname   string   `json:"nickname"`
+	sex        int      `json:"sex"`
+	province   string   `json:"province"`
+	city       string   `json:"city"`
+	country    string   `json:"country"`
+	headimgurl string   `json:"headimgurl"`
+	privilege  []string `json:"privilege"`
+	unionid    string   `json:"unionid"`
+}
+
+func (u *UserInfo) GetToken(code string) *UserToken {
+
+	url := "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code"
+	cache := new(utils.Cache)
+
+	ut := new(UserToken)
+
+	appid := cache.Get(utils.WECHAT_APPID)
+	secret := cache.Get(utils.WECHAT_APPSECRET)
+
+	url = strings.Replace(url, "APPID", appid, -1)
+	url = strings.Replace(url, "SECRET", secret, -1)
+	url = strings.Replace(url, "CODE", code, -1)
+
+	log4go.Debug(url)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return ut
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	log4go.Debug(string(body))
+
+	json.Unmarshal(body, &ut)
+
+	return ut
+}
+
+func (u *UserInfo) GetUserInfo(openid, token string) *UserInfo {
+
+	url := "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN"
+	user := new(UserInfo)
+
+	url = strings.Replace(url, "ACCESS_TOKEN", token, -1)
+	url = strings.Replace(url, "OPENID", openid, -1)
+
+	log4go.Debug(url)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return user
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	log4go.Debug(string(body))
+
+	json.Unmarshal(body, &user)
+
+	cache := new(utils.Cache)
+	cache.Set(utils.WECHAT_USER_INFO+user.openid, string(body), 7200)
+
+	return user
+}
